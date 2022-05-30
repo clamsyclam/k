cf56f5b79baf0f445ea2902d179e7e459046ade4
clamsyclam <105502564+clamsyclam@users.noreply.github.com>
2022-05-30 13:18:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,6 @@ PROPORTIONAL_GAIN = 1.2
 
 while True:
    
-    left_motor.run_angle(1500, 3000)
-    left_motor.run_angle(1500, -3000)
+    left_motor.run_angle(1500, 2500)
+    left_motor.run_angle(1500, -2500)
     
